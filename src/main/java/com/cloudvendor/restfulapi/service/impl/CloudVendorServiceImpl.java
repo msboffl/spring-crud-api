297d5536b7f9edae1b3aec03217f028c52510510
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-04-14 18:54:46 +0530

    Rest Api

---
+++ src/main/java/com/cloudvendor/restfulapi/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,45 @@
+package com.cloudvendor.restfulapi.service.impl;
+
+import com.cloudvendor.restfulapi.model.CloudVendor;
+import com.cloudvendor.restfulapi.repository.CloudVendorRepository;
+import com.cloudvendor.restfulapi.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+    CloudVendorRepository cloudVendorRepository;
+
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRepository.findAll();
+    }
+}
